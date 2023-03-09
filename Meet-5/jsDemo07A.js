3beb76d85cdef186b54a3da1f7b4ca2627bddd92
Mjulianfr001056 <222112217@stis.ac.id>
2023-03-09 08:38:58 +0700

    Add .toFixed(3) Method

--- Meet-5/jsDemo07A.js
+++ Meet-5/jsDemo07A.js
@@ -1,8 +1,8 @@
 for (var ii = 4, jj = 3; jj >= 0; ii++, jj--) { 
-    document.writeln(ii + " * " + jj + " = " + ii * jj + "<br>") 
-    document.writeln(ii + " / " + jj + " = " + ii / jj + "<br>") 
+    document.writeln(ii + " * " + jj + " = " + (ii * jj ).toFixed(3) + "<br>") 
+    document.writeln(ii + " / " + jj + " = " + (ii / jj).toFixed(3) + "<br>") 
     document.writeln("log(" + jj + ") = " + 
-      Math.log(jj) + "<br>") 
+      Math.log(jj).toFixed(3) + "<br>") 
     document.writeln("sqrt(" + (jj - 1) + ") = " + 
-      Math.sqrt(jj - 1) + "<br><br>") 
+      Math.sqrt(jj - 1).toFixed(3) + "<br><br>") 
   }
